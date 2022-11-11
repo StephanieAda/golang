81816f0069d12d88987c0d8f91db714433e034bf
StephanieAda <elluhstephanie@gmail.com>
2022-11-11 23:02:59 +0100

    a time

--- 12switchcase/main.go
+++ 12switchcase/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("Welcome to switchcase")
+
+	rand.Seed(time.Now().UnixNano())
+	diceNum := rand.Intn(6) + 1
+	fmt.Println("Value of dice is", diceNum)
+
+	switch diceNum {
+	case 1:
+		fmt.Println("Dice value is 1, you can open")
+
+	case 2:
+		fmt.Println("Dice value is 2, you can move 2 paces")
+
+	case 3:
+		fmt.Println("Dice value is 3, you can move 3 paces")
+		fallthrough
+	case 4:
+		fmt.Println("Dice value is 4, you can move 4 paces")
+
+	case 5:
+		fmt.Println("Dice value is 5, you can move 5 paces")
+
+	case 6:
+		fmt.Println("Dice value is 6, you can move 6 paces or play again")
+
+	default:
+		fmt.Println("What was that?!")
+
+	}
 }
---
+++ 13loops/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to loops in golang")
+
+	days := []string{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"}
+
+	fmt.Println(days)
+
+	// for i := 0; i < len(days); i++ {
+	// 	fmt.Println(days[i])
+	// }
+
+	// for i := range days {
+	// 	fmt.Println(days[i])
+	// }
+
+	// for index, value := range days {
+	// 	fmt.Printf("index is %v and value is %v \n", index, value)
+	// }
+
+	rougeValue := 1
+
+	for rougeValue < 10 {
+
+		if rougeValue == 2 {
+			goto lco
+		}
+		if rougeValue == 5 {
+			//break
+			rougeValue++
+			continue
+		}
+		fmt.Println("the real real Value is: ", rougeValue)
+		rougeValue++
+	}
+
+lco:
+	fmt.Println("Jumping at a goat")
+}
